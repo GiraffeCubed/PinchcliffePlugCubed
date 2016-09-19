8cebca2c67f94daeebb4f3b0dbf98291651f24ad
GiraffeCubed <jordan.mccormack261@gmail.com>
2016-09-19 21:47:52 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -25,15 +25,15 @@
         }
 
         // Example code for a bot command:
-        bot.commands.jamCommand = {
-          command: 'jam',  // The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.baconCommand = {
+          command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
           rank: 'user', // Minimum user permission to use the command
           type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("https://c1.staticflickr.com/9/8353/8304886007_c49bceaa8f_z.jpg"");
+              API.sendChat("/me Bacon!!!");
             }
           }
         };
@@ -41,7 +41,7 @@
         // Load the chat package again to account for any changes
         bot.loadChat();
 
-      }
+}
 
     //Change the bots default settings and make sure they are loaded on launch
 
